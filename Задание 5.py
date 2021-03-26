7e4025d13dcd70057d764d11c3b5c8cb86a36ff4
Kairienn <scorpion942@yandex.ru>
2021-03-26 18:16:37 +0300

    Домашнее задание к 5 уроку

--- Задание 5.py
+++ Задание 5.py
@@ -1,10 +1,14 @@
-from functools import reduce
-my_list = [n for n in range(100, 1001) if n % 2 == 0]
-print(my_list)
+def summary():
+    try:
+        with open('my_file_5.txt', 'w+') as file_obj:
+            line = input('Введите цифры, разделенные пробелами \n')
+            file_obj.writelines(line)
+            number = line.split()
+            print(sum(map(int, number)))
+    except IOError:
+        print('Ошибка в файле')
+    except ValueError:
+        print('Неправильный номер.')
 
 
-def my_func(n_prev, n):
-    return n_prev*n
-
-
-print(reduce(my_func, my_list))
+summary()
