2159da0de6676f697c65976f260d1b9cb7ad99e2
Kairienn <scorpion942@yandex.ru>
2021-03-26 18:16:44 +0300

    Домашнее задание к 5 уроку

--- Задание 7.py
+++ Задание 7.py
@@ -1,17 +1,28 @@
-from itertools import count
-from math import factorial
-
-
-def fact():
-    for x in count(1):
-        yield factorial(x)
-
-
-crie = fact()
+import json
+profit = {}
+gain = {}
+prof = 0
+prof_average = 0
 counter = 0
-for z in crie:
-    if counter < 7:
-        print(z)
-        counter += 1
+with open('my_file_7.txt', 'r') as file:
+    for l in file:
+        name, firm, earning, costs = l.split()
+        profit[name] = int(earning) - int(costs)
+        if profit.setdefault(name) >= 0:
+            prof = prof + profit.setdefault(name)
+            counter += 1
+    if counter != 0:
+        prof_average = prof / counter
+        print(f'Прибыль средняя - {prof_average:.2f}')
     else:
-        break
+        print(f'Прибыль средняя - отсутсвует. Все работают в убыток')
+    gain = {'средняя прибыль': round(prof_average)}
+    profit.update(gain)
+    print(f'Прибыль каждой компании - {profit}')
+
+with open('file_7.json', 'w') as write_js:
+    json.dump(profit, write_js)
+
+    js_str = json.dumps(profit)
+    print(f'Создан файл с расширением json со следующим содержимым: \n '
+          f' {js_str}')
