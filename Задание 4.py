19da43b8bae5569e33dd63903e958eaabb434367
Kairienn <scorpion942@yandex.ru>
2021-03-26 18:16:33 +0300

    Домашнее задание к 5 уроку

--- Задание 4.py
+++ Задание 4.py
@@ -1,3 +1,12 @@
-my_list = [1, 145, 1, 8, 4, 32, 27, 4, 145, 7, 33]
-my_new_list = [n for n in my_list if my_list.count(n) == 1]
-print(my_new_list)
+rus = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
+new_file = []
+with open("my_file_4.txt", "r") as file:
+    for l in file:
+        l = l.split(" ", 1)
+        new_file.append(rus[l[0]] + " " + l[1])
+    print(new_file)
+
+with open("my_file_4_new.txt", "w") as file_2:
+    file_2.writelines(new_file)
+
+
