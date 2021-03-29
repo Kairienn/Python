9dd54830efd1e08b7503ae9a2858788ad41f3143
Kairienn <scorpion942@yandex.ru>
2021-03-29 16:58:38 +0300

    Домашнее задание к 6 уроку

--- Задание 4.py
+++ Задание 4.py
@@ -1,12 +1,78 @@
-rus = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
-new_file = []
-with open("my_file_4.txt", "r") as file:
-    for l in file:
-        l = l.split(" ", 1)
-        new_file.append(rus[l[0]] + " " + l[1])
-    print(new_file)
+class Car:
+    # atributes
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
 
-with open("my_file_4_new.txt", "w") as file_2:
-    file_2.writelines(new_file)
+    # methods
+    def go(self):
+        return f'{self.name} is started'
+
+    def stop(self):
+        return f'{self.name} is stopped'
+
+    def turn_right(self):
+        return f'{self.name} is turned right'
+
+    def turn_left(self):
+        return f'{self.name} is turned left'
+
+    def show_speed(self):
+        return f'Current speed {self.name} is {self.speed}'
 
 
+class TownCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Current speed of town car {self.name} is {self.speed}')
+
+        if self.speed > 40:
+            return f'Speed of {self.name} is higher than allow for town car'
+        else:
+            return f'Speed of {self.name} is normal for town car'
+
+class SportCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+class WorkCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Current speed of work car {self.name} is {self.speed}')
+
+        if self.speed > 60:
+            return f'Speed of {self.name} is higher than allow for work car'
+
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def police(self):
+        if self.is_police:
+            return f'{self.name} is from police department'
+        else:
+            return f'{self.name} is not from police department'
+
+
+dodge = SportCar(120, 'Red', 'Dodge', False)
+oka = TownCar(40, 'White', 'Oka', False)
+lada = WorkCar(80, 'Rose', 'Lada', True)
+ford = PoliceCar(130, 'Blue',  'Ford', True)
+print(lada.turn_left())
+print(f'When {oka.turn_right()}, then {dodge.stop()}')
+print(f'{lada.go()} with speed exactly {lada.show_speed()}')
+print(f'{lada.name} is {lada.color}')
+print(f'Is {dodge.name} a police car? {dodge.is_police}')
+print(f'Is {lada.name}  a police car? {lada.is_police}')
+print(dodge.show_speed())
+print(oka.show_speed())
+print(ford.police())
+print(ford.show_speed())
